81a3a2140f9b43479a74f7e23f841f7b1469162b
vats <shivamvats1610@gmail.com>
2024-01-11 11:36:25 +0530

    hello

---
+++ src/Secondprog.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Secondprog {
+    public static void main(String[] args){
+        System.out.println("Hello vats");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
